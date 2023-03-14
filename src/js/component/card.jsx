a345ac0b568d3813b63ccb81a9ae2d2252a556eb
LesCamposR <lesthercamposramirez91@gmail.com>
2023-03-14 17:20:24 +0000

    Fist

---
+++ src/js/component/card.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Honey from "../../img/Honey.png";
+
+const Card = (props) => {
+    return (
+      <>
+        <div className="col" key={props.key}>
+          <div className="card shadow-sm">
+            <img
+              src={Honey}
+              className="bd-placeholder-img card-img-top"
+              width="100%"
+              height="225"
+            />
+  
+            <div className="card-body text-center">
+              <h5 className="card-title">{props.titulo}</h5>
+              <p className="card-text">{props.descripcion}</p>
+            </div>
+            <div className="card-footer text-center">
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  };
+  
+  export default Card;
+  
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,58 @@
 import React from "react";
 
+
+
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Honey from "../../img/Honey.png";
+import Navbar from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
+import Card from "./card.jsx";
+import Footer from "./footer.jsx";
+
 
 //create your first component
 const Home = () => {
+	let cardContent = [
+		{
+			titulo: "Costa Rica",
+			noticia: "100% Raw Honey is being Exported by local producers",
+		},
+		{
+			titulo: "Surfing",
+			noticia: "Helps people dealing with stress",
+		},
+		{
+			titulo: "Fishing",
+			noticia: "Fishermen are happy",
+		},
+		{
+			titulo: "Photography",
+			noticia: "Gallery exposition tomorrow",
+		},
+	];
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+		<>
+			<Navbar />
+			<Jumbotron />
+			<div className="album py-3">
+				<div className="container">
+					<div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-4">
+						{cardContent.map((item, index) => {
+							return (
+								<Card
+									titulo={item.titulo}
+									descripcion={item.noticia}
+									key={index}
+
+								/>
+							);
+						})}
+					</div>
+				</div>
+			</div>
+			<Footer />
+		</>)
+
 };
 
 export default Home;
